ec88fa23ff817230764d81b7364fbb0d72dae0b1
Samoolino <samo4au@gmail.com>
2026-08-14 08:05:31 +0100

    fix: remove duplicate vendor settlement settled_at column

--- db/migrate/20260814000005_extend_oja_vendor_settlements_for_payouts.rb
+++ db/migrate/20260814000005_extend_oja_vendor_settlements_for_payouts.rb
@@ -2,7 +2,6 @@ class ExtendOjaVendorSettlementsForPayouts < ActiveRecord::Migration[7.2]
   def change
     add_column :oja_vendor_settlements, :payout_reference, :string
     add_column :oja_vendor_settlements, :destination_reference, :string
-    add_column :oja_vendor_settlements, :settled_at, :datetime
     add_index :oja_vendor_settlements, :payout_reference, unique: true
   end
 end
