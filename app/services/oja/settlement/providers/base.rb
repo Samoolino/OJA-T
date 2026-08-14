3dce56a5f1ae8e2fa0be9beb8bebe4dba932d0ae
Samoolino <samo4au@gmail.com>
2026-08-14 22:42:40 +0100

    fix: make payout provider destination optional at base contract

---
+++ app/services/oja/settlement/providers/base.rb
@@ -0,0 +1,19 @@
+module Oja
+  class Settlement
+    module Providers
+      class Base
+        def create_payout(settlement:, connected_account_id: nil)
+          raise NotImplementedError
+        end
+
+        def fetch_payout(reference:)
+          raise NotImplementedError
+        end
+
+        def cancel_payout(reference:)
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end
