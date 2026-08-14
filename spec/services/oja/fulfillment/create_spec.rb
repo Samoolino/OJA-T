0794d6a317cce5dfcf9e1c1e9defc24d8dd5534c
Samoolino <samo4au@gmail.com>
2026-08-14 10:21:43 +0100

    test: align fulfillment specs with order-level service contract

--- spec/services/oja/fulfillment/create_spec.rb
+++ spec/services/oja/fulfillment/create_spec.rb
@@ -1,38 +1,45 @@
-require "spec_helper"
+require "rails_helper"
 
 RSpec.describe Oja::Fulfillment::Create do
-  it "creates one vendor fulfillment per idempotency key" do
+  it "creates one vendor fulfillment per order allocation" do
     order = instance_double("Spree::Order", id: 101)
+    order_allocation = instance_double(Oja::OrderAllocation, vendor_reference: "vendor-1")
+    profile = instance_double(Oja::VendorFulfillmentProfile, fulfillment_mode: "courier")
     fulfillment = instance_double(Oja::OrderFulfillment)
+    allocations = double("OrderAllocationsRelation")
 
-    allow(Oja::OrderFulfillment).to receive(:find_or_create_by!).with(idempotency_key: "fulfill-1").and_yield(fulfillment).and_return(fulfillment)
-    allow(fulfillment).to receive(:order_id=)
-    allow(fulfillment).to receive(:vendor_reference=)
+    allow(ApplicationRecord).to receive(:transaction).and_yield
+    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 101).and_return(allocations)
+    allow(allocations).to receive(:find_each).and_yield(order_allocation)
+    allow(Oja::VendorFulfillmentProfile).to receive(:find_by).with(vendor_reference: "vendor-1").and_return(profile)
+    allow(Oja::OrderFulfillment).to receive(:create_or_find_by!).with(
+      order_id: 101,
+      vendor_reference: "vendor-1"
+    ).and_yield(fulfillment).and_return(fulfillment)
+
     allow(fulfillment).to receive(:fulfillment_mode=)
     allow(fulfillment).to receive(:status=)
-    allow(fulfillment).to receive(:tracking_reference=)
+    allow(fulfillment).to receive(:idempotency_key=)
 
-    expect(described_class.call(
-      order: order,
-      vendor_reference: "vendor-1",
-      mode: :courier,
-      idempotency_key: "fulfill-1"
-    )).to eq(fulfillment)
-
-    expect(fulfillment).to have_received(:order_id=).with(101)
-    expect(fulfillment).to have_received(:vendor_reference=).with("vendor-1")
+    expect(described_class.call(order: order, idempotency_key: "fulfill-1")).to eq(fulfillment)
     expect(fulfillment).to have_received(:fulfillment_mode=).with("courier")
     expect(fulfillment).to have_received(:status=).with("pending")
+    expect(fulfillment).to have_received(:idempotency_key=).with("fulfill-1:fulfillment:0")
   end
 
   it "rejects unsupported fulfillment modes" do
+    order = instance_double("Spree::Order", id: 101)
+    order_allocation = instance_double(Oja::OrderAllocation, vendor_reference: "vendor-1")
+    profile = instance_double(Oja::VendorFulfillmentProfile, fulfillment_mode: "unsupported")
+    allocations = double("OrderAllocationsRelation")
+
+    allow(ApplicationRecord).to receive(:transaction).and_yield
+    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 101).and_return(allocations)
+    allow(allocations).to receive(:find_each).and_yield(order_allocation)
+    allow(Oja::VendorFulfillmentProfile).to receive(:find_by).with(vendor_reference: "vendor-1").and_return(profile)
+
     expect {
-      described_class.call(
-        order: instance_double("Spree::Order", id: 101),
-        vendor_reference: "vendor-1",
-        mode: :unsupported,
-        idempotency_key: "fulfill-2"
-      )
+      described_class.call(order: order, idempotency_key: "fulfill-2")
     }.to raise_error(ArgumentError, "invalid fulfillment mode")
   end
 end
