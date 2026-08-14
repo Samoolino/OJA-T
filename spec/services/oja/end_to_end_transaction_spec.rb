a7419368bbb74ee455c08771db31fa248fb805be
Samoolino <samo4au@gmail.com>
2026-08-14 10:51:39 +0100

    test: tighten end-to-end fulfillment lifecycle doubles

--- spec/services/oja/end_to_end_transaction_spec.rb
+++ spec/services/oja/end_to_end_transaction_spec.rb
@@ -24,10 +24,6 @@ RSpec.describe "OJA end-to-end transaction lifecycle" do
       { allocation: allocation_b, reservation: reservation_b, amount: 40_000, vendor_reference: "vendor-b", settlement_profile: profile_b }
     ]
 
-    fulfillment_a = instance_double(Oja::OrderFulfillment, order_id: 501, vendor_reference: "vendor-a", delivered?: false)
-    settlement_a = instance_double(Oja::VendorSettlement,
-      order_allocation: order_allocation_a, payout_reference: "payout-a", paid?: false, failed?: false)
-
     allow(ApplicationRecord).to receive(:transaction).and_yield
     allow(payment_intent).to receive(:with_lock).and_yield
     expect(payment_intent).to receive(:update!).with(status: :order_pending, order: order)
@@ -48,19 +44,35 @@ RSpec.describe "OJA end-to-end transaction lifecycle" do
       allocation_parts: parts
     )
 
-    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 501, vendor_reference: "vendor-a").and_return(double(select: double))
-    allow(Oja::VendorFulfillmentProfile).to receive(:find_by).with(vendor_reference: "vendor-a").and_return(profile_a)
-    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 501).and_return(double(each_with_index: nil))
+    fulfillment_a = instance_double(Oja::OrderFulfillment)
+    fulfillment_b = instance_double(Oja::OrderFulfillment)
+    allow(fulfillment_a).to receive(:fulfillment_mode=)
+    allow(fulfillment_a).to receive(:status=)
+    allow(fulfillment_a).to receive(:idempotency_key=)
+    allow(fulfillment_b).to receive(:fulfillment_mode=)
+    allow(fulfillment_b).to receive(:status=)
+    allow(fulfillment_b).to receive(:idempotency_key=)
 
-    expect(Oja::OrderFulfillment).to receive(:create_or_find_by!).with(order_id: 501, vendor_reference: "vendor-a").and_yield(double(fulfillment_mode: nil, status: nil, idempotency_key: nil))
-    allow(Oja::OrderFulfillment).to receive(:create_or_find_by!).with(order_id: 501, vendor_reference: "vendor-b").and_return(double)
+    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 501).and_return([order_allocation_a, order_allocation_b])
+    allow(Oja::VendorFulfillmentProfile).to receive(:find_by).with(vendor_reference: "vendor-a").and_return(profile_a)
+    allow(Oja::VendorFulfillmentProfile).to receive(:find_by).with(vendor_reference: "vendor-b").and_return(profile_b)
+    expect(Oja::OrderFulfillment).to receive(:create_or_find_by!).with(order_id: 501, vendor_reference: "vendor-a").and_yield(fulfillment_a).and_return(fulfillment_a)
+    expect(Oja::OrderFulfillment).to receive(:create_or_find_by!).with(order_id: 501, vendor_reference: "vendor-b").and_yield(fulfillment_b).and_return(fulfillment_b)
 
     Oja::Fulfillment::Create.call(order: order, idempotency_key: "fulfill-1")
 
+    allow(fulfillment_a).to receive(:delivered?).and_return(false)
+    allow(fulfillment_a).to receive(:order_id).and_return(501)
+    allow(fulfillment_a).to receive(:vendor_reference).and_return("vendor-a")
     allow(fulfillment_a).to receive(:with_lock).and_yield
     allow(fulfillment_a).to receive(:update!).with(hash_including(status: "delivered"))
-    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 501, vendor_reference: "vendor-a").and_return(double(select: [order_allocation_a.id]))
-    allow(Oja::VendorSettlement).to receive(:where).and_return(double(find_each: [settlement_a]))
+
+    settlement_a = instance_double(Oja::VendorSettlement, order_allocation: order_allocation_a, paid?: false, failed?: false)
+    settlement_relation = double("PendingSettlements")
+    allocation_scope = double("OrderAllocationScope", select: [101])
+    allow(Oja::OrderAllocation).to receive(:where).with(order_id: 501, vendor_reference: "vendor-a").and_return(allocation_scope)
+    allow(Oja::VendorSettlement).to receive(:where).with(order_allocation_id: [101], status: :pending).and_return(settlement_relation)
+    allow(settlement_relation).to receive(:find_each).and_yield(settlement_a)
     allow(settlement_a).to receive(:update!).with(status: :payable)
     expect(Oja::Settlement::Route).to receive(:call).with(settlement: settlement_a, profile: profile_a)
 
@@ -70,9 +82,6 @@ RSpec.describe "OJA end-to-end transaction lifecycle" do
     allow(settlement_a).to receive(:with_lock).and_yield
     expect(settlement_a).to receive(:update!).with(status: :paid, settled_at: kind_of(Time))
 
-    Oja::Settlement::Reconcile.call(
-      external_reference: "payout-a",
-      event_type: "transfer.success"
-    )
+    Oja::Settlement::Reconcile.call(external_reference: "payout-a", event_type: "transfer.success")
   end
 end
