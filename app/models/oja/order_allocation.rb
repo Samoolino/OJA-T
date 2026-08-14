7027b7a47f21b98abc996669fb484019e637ea0c
Samoolino <samo4au@gmail.com>
2026-08-14 03:29:58 +0100

    feat: associate order allocations with vendor settlement profiles

--- app/models/oja/order_allocation.rb
+++ app/models/oja/order_allocation.rb
@@ -4,8 +4,10 @@ module Oja
 
     belongs_to :allocation, class_name: "Oja::PlanAllocation"
     belongs_to :reservation, class_name: "Oja::AllocationReservation"
+    belongs_to :settlement_profile, class_name: "Oja::VendorSettlementProfile"
+    has_many :vendor_settlements, class_name: "Oja::VendorSettlement", dependent: :restrict_with_exception
 
-    validates :amount, :currency, presence: true
+    validates :amount, :currency, :vendor_reference, presence: true
     validates :amount, numericality: { greater_than: 0 }
   end
 end
