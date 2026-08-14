a92fd46d7e33e1cdb27d397a6a75f7dbab1be0ca
Samoolino <samo4au@gmail.com>
2026-08-14 04:26:49 +0100

    feat: link payment intents to Spree order lifecycle

---
+++ db/migrate/20260814040000_add_transaction_lineage_to_oja_payment_intents.rb
@@ -0,0 +1,14 @@
+class AddTransactionLineageToOjaPaymentIntents < ActiveRecord::Migration[7.2]
+  def change
+    change_table :oja_payment_intents, bulk: true do |t|
+      t.bigint :order_id
+      t.string :idempotency_key
+      t.datetime :authorized_at
+      t.datetime :completed_at
+    end
+
+    add_index :oja_payment_intents, :order_id, unique: true
+    add_index :oja_payment_intents, :idempotency_key, unique: true
+    add_foreign_key :oja_payment_intents, :spree_orders, column: :order_id
+  end
+end
