26d9a1257ffe5e4afa369f2e32db452071ab4dbe
Samoolino <samo4au@gmail.com>
2026-08-14 07:59:14 +0100

    fix: remove placeholder settlement model constants

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,6 @@ require "bigdecimal/util"
 ROOT = File.expand_path("..", __dir__)
 $LOAD_PATH.unshift(File.join(ROOT, "app"))
 
-module Oja
-  class VendorSettlement; end unless const_defined?(:VendorSettlement)
-  class VendorSettlementProfile; end unless const_defined?(:VendorSettlementProfile)
-end
-
 require "services/oja/settlement/scheduler"
 require "services/oja/settlement/route"
 require "services/oja/settlement/transfer_gateway"
