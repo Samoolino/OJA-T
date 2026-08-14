746823d1ba97b9ed7948dd360daae478ccbac589
Samoolino <samo4au@gmail.com>
2026-08-14 19:54:58 +0100

    feat: add dedicated Stripe webhook route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "webhooks/payment", to: "webhooks#payment"
+      post "webhooks/stripe", to: "webhooks#stripe"
     end
   end
 end
