36c56fb064ed89f4aba2ad7197acc3d8b9bfc351
Samoolino <samo4au@gmail.com>
2026-09-20 17:53:51 +0100

    feat: make OJA-T financial ledger immutable

--- app/models/oja/financial_ledger_entry.rb
+++ app/models/oja/financial_ledger_entry.rb
@@ -1,17 +1,16 @@
 module Oja
-  class FinancialLedgerEntry
-    include ActiveModel::Model
-    include ActiveModel::Attributes
-    attribute :id, :integer
-    attribute :allocation_id, :integer
-    attribute :operation_id, :string
-    attribute :idempotency_key, :string
-    attribute :entry_type, :string
-    attribute :amount_minor, :integer
-    attribute :currency, :string
-    attribute :correlation_id, :string
-    attribute :source_type, :string
-    attribute :source_id, :string
-    attribute :metadata, :value
+  class FinancialLedgerEntry < ActiveRecord::Base
+    self.table_name = "oja_financial_ledger_entries"
+
+    belongs_to :allocation, class_name: "Oja::PlanAllocation", foreign_key: :allocation_id, inverse_of: :ledger_entries
+
+    ENTRY_TYPES = %w[fund reserve consume release reverse].freeze
+
+    validates :operation_id, :idempotency_key, :entry_type, :currency, :correlation_id, presence: true
+    validates :amount_minor, numericality: { greater_than: 0 }
+    validates :entry_type, inclusion: { in: ENTRY_TYPES }
+
+    before_update { raise ActiveRecord::ReadOnlyRecord, "financial ledger entries are immutable" }
+    before_destroy { raise ActiveRecord::ReadOnlyRecord, "financial ledger entries are immutable" }
   end
 end
